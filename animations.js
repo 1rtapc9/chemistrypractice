0aa3dfdd892c6df123653e1dbb0f6aaecbe21e0a
1rtapc9 <arantrydert@gmail.com>
2025-11-12 13:18:30 -0500

    Update animations.js

--- animations.js
+++ animations.js
@@ -7,9 +7,19 @@ function spawnFloatingMolecule(){
   document.body.appendChild(mol);
   setTimeout(()=>mol.remove(),4000);
 }
-
-// Use for streak milestones
 function celebrateStreak(){
-  spawnFloatingMolecule();
-  spawnConfetti(20);
+  for(let i=0;i<10;i++) spawnFloatingMolecule();
+  spawnConfetti(30);
+}
+function spawnConfetti(count){
+  for(let i=0;i<count;i++){
+    const conf = document.createElement("div");
+    conf.textContent = "🎉";
+    conf.style.position="absolute";
+    conf.style.left=Math.random()*100+"%";
+    conf.style.top=Math.random()*100+"%";
+    conf.style.fontSize=(Math.random()*20+10)+"px";
+    document.body.appendChild(conf);
+    setTimeout(()=>conf.remove(),3000);
+  }
 }
