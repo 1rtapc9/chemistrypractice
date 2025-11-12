d07931124f2bda32152a4934a20d04d8d50a9cb6
1rtapc9 <arantrydert@gmail.com>
2025-11-12 12:54:09 -0500

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,17 @@
+const ARTICLES=[
+  "Did you know water expands when frozen?",
+  "Carbon dioxide is a greenhouse gas.",
+  "The periodic table was first published in 1869.",
+  "Acids turn blue litmus red.",
+  "Bases turn red litmus blue.",
+  "Sodium reacts violently with water.",
+  "Chlorine gas was used in WWI.",
+  "Hydrocarbons are the main components of fuels.",
+  "Ethanol is a common alcohol.",
+  "Covalent bonds share electrons.",
+  "Ionic bonds transfer electrons.",
+  "Acids donate protons in reactions.",
+  "Bases accept protons in reactions.",
+  "The pH scale ranges from 0 to 14.",
+  "Hydrogen is the lightest element."
+];
